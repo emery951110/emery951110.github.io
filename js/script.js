0098d98772fbea71028eb39a0063274a733690da
Emery Lin <75047929+emery951110@users.noreply.github.com>
2022-10-28 21:14:16 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@
  *
  */
 document.getElementById("my_audio").play();
-console.log('Shaadi me zaroor aana');
 
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 3, 2022 00:00:00").getTime();
