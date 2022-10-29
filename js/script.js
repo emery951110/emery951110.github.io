112aae693ae36fa6678e7bdf67651abb314ef3a1
Emery Lin <75047929+emery951110@users.noreply.github.com>
2022-10-30 00:16:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,12 +32,13 @@ var countDownDate = new Date("Oct 30, 2022 00:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
-
+    clearInterval(x)
     // Get todays date and time
     var now = new Date().getTime();
     
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    //var distance = countDownDate - now;
+    var distance = Math.abs(countDownDate - now);
     
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
