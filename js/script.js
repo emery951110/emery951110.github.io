cc9d9d4a77d7ee8e32733e909fb2714ea7af5384
Emery Lin <75047929+emery951110@users.noreply.github.com>
2022-10-30 00:34:21 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 30, 2022 00:32:00").getTime();
+var countDownDate = new Date("Oct 30, 2022 00:36:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
