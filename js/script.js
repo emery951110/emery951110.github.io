e183af8201f8c417a13b04a12306d916c79f3c5d
Emery Lin <75047929+emery951110@users.noreply.github.com>
2022-10-29 23:26:34 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
  */
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
-    var vid = document.getElementById("my_");
+    var vid = document.getElementById("my_audio");
     vid.volume = 0.2;
     console.log('Shaadi me zaroor aana');
 });
